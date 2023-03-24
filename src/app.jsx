24bd1dafbf2dfd90cbfd2f541b2b032ad0830ff9
yejinzhan <jinzhan.ye@foxmail.com>
2023-03-25 00:08:02 +0800

    chore: 打稿

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import './app.css';
 import Home from './pages/home';
-import About from './pages/about';
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/home" element={<Home />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,87 @@
-import { Link } from 'react-router-dom';
-
-import logo from '../logo.svg';
-
 function Home() {
+  const hashVal = '00000000000000000007878ec04bb2b2e12317804810f4c26033585b3f81ffaa';
+  fetch(`https://blockchain.info/rawblock/${hashVal}`)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log('result', result);
+    });
+
   return (
-    <header className="app-header">
-      <img src={logo} className="app-logo" alt="logo" />
-      <pre style={{ textAlign: 'left' }}>
-        <code>window.blocklet = {JSON.stringify(window.blocklet, null, 2)}</code>
-      </pre>
-      <Link className="app-link" to="/about">
-        About
-      </Link>
-      <a className="app-link" href="https://developer.blocklet.io/docs/" target="_blank" rel="noopener noreferrer">
-        Learn Blocklet
-      </a>
-    </header>
+    <div>
+      <div>
+        <input type="text" />
+        <div className="search-button">Search</div>
+      </div>
+
+      <div className="list">
+        <div className="list-item">
+          <div className="list-item-row-1">
+            <div className="avatar" />
+
+            <div className="list-item-info">
+              <div>
+                <div>
+                  <div>0</div>
+                  <div>ID:</div>
+                  <div>aba-123</div>
+                </div>
+
+                <div>12/22/2020, 15:09:42</div>
+              </div>
+
+              <div>
+                <div>
+                  <div>From</div>
+                  <div>Block Reward</div>
+                </div>
+
+                <div>
+                  <div>To</div>
+                  <div>4 Outputs</div>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <div>6.41583560 BTC</div>
+                </div>
+
+                <div>
+                  <div>Fee</div>
+                  <div>0 Sats</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="icon-arrow" />
+          </div>
+
+          <div className="list-item-row-2">
+            <div>
+              <strong>From</strong>
+              <div>
+                <div>1</div>
+                <div>
+                  <div>1BL1Y3VjyRBM44vr2parCXb9HquGDU4bdA</div>
+                  <div>0.00506000 BTC</div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <strong>To</strong>
+              <div className="flex-row">
+                <div>1</div>
+                <div>
+                  <div>1BL1Y3VjyRBM44vr2parCXb9HquGDU4bdA</div>
+                  <div>0.00506000 BTC</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
